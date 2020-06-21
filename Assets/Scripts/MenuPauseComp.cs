67c62a22551e78c502aa071f54dcc8a47fd024a8
Daniel Assis <daniel.assis@inatel.br>
2020-06-21 19:33:05 -0300

    Add MenuPause and MenuPrincipal

---
+++ Assets/Scripts/MenuPauseComp.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPauseComp : MonoBehaviour
+{
+    public static bool pausado;
+
+    public GameObject menuPausePanel;
+
+    /// <summary>
+    /// Metodo para reiniciar a scene
+    /// </summary>
+    public void Restart()
+    {
+        print("teste");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    /// <summary>
+    /// Metodo para pausar o jogo
+    /// </summary>
+    public void Pause(bool isPausado)
+    {
+        pausado = isPausado;
+
+        //Se o jogo estiver pausado, timescale recebe 0
+        Time.timeScale = (pausado) ? 0 : 1;
+
+        menuPausePanel.SetActive(pausado);
+    }
+
+    /// <summary>
+    /// Metodo para carregar uma scene
+    /// </summary>
+    public void CarregaScene(string nomeScene)
+    {
+        SceneManager.LoadScene(nomeScene);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Pause(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    /// <summary>
+    /// Metodo para carregar uma scene
+    /// </summary>
+    public void CarregaScene(string nomeScene)
+    {
+        SceneManager.LoadScene(nomeScene);
+    }
+}
